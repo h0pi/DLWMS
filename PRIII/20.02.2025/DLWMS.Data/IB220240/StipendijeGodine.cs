eb6af0c059d95abac3a32d8afa0112f6684984c0
h0pi <ammy.hopi@gmail.com>
2025-03-22 00:05:38 +0100

    Update

---
+++ PRIII/20.02.2025/DLWMS.Data/IB220240/StipendijeGodine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLWMS.Data.IB220240
+{
+    public class StipendijeGodine
+    {
+        public int Id { get; set; }
+        public int Godina { get; set; }
+        public int StipendijaId { get; set; }
+        public Stipendije Stipendija { get; set; }
+        public int Iznos { get; set; }
+    }
+}
---
+++ PRIII/20.02.2025/DLWMS.Data/IB220240/StudentiStipendije.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLWMS.Data.IB220240
+{
+    public class StudentiStipendije
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public int StipendijeGodineId { get; set; }
+        public StipendijeGodine StipendijeGodine { get; set; }
+    }
+}
