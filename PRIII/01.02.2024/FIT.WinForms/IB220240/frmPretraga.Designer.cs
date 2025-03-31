035e3b0571c21a19e33a194119beccc1a3b4388d
h0pi <ammy.hopi@gmail.com>
2025-03-31 22:29:45 +0200

    Uradjen ispit bez reporta

--- PRIII/01.02.2024/FIT.WinForms/IB220240/frmPretraga.Designer.cs
+++ PRIII/01.02.2024/FIT.WinForms/IB220240/frmPretraga.Designer.cs
@@ -28,12 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmPretraga";
+            label1 = new Label();
+            cmbDrzava = new ComboBox();
+            label2 = new Label();
+            cmbGrad = new ComboBox();
+            dgvPodaci = new DataGridView();
+            Ime = new DataGridViewTextBoxColumn();
+            Prezime = new DataGridViewTextBoxColumn();
+            Grad = new DataGridViewTextBoxColumn();
+            Drzava = new DataGridViewTextBoxColumn();
+            Prosjek = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dgvPodaci).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Drzava:";
+            // 
+            // cmbDrzava
+            // 
+            cmbDrzava.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbDrzava.FormattingEnabled = true;
+            cmbDrzava.Location = new Point(12, 32);
+            cmbDrzava.Name = "cmbDrzava";
+            cmbDrzava.Size = new Size(192, 28);
+            cmbDrzava.TabIndex = 1;
+            cmbDrzava.SelectionChangeCommitted += cmbDrzava_SelectionChangeCommitted;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(214, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Grad:";
+            // 
+            // cmbGrad
+            // 
+            cmbGrad.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbGrad.FormattingEnabled = true;
+            cmbGrad.Location = new Point(214, 32);
+            cmbGrad.Name = "cmbGrad";
+            cmbGrad.Size = new Size(192, 28);
+            cmbGrad.TabIndex = 1;
+            cmbGrad.SelectionChangeCommitted += cmbGrad_SelectionChangeCommitted;
+            // 
+            // dgvPodaci
+            // 
+            dgvPodaci.AllowUserToAddRows = false;
+            dgvPodaci.AllowUserToDeleteRows = false;
+            dgvPodaci.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvPodaci.Columns.AddRange(new DataGridViewColumn[] { Ime, Prezime, Grad, Drzava, Prosjek });
+            dgvPodaci.Location = new Point(12, 77);
+            dgvPodaci.Name = "dgvPodaci";
+            dgvPodaci.ReadOnly = true;
+            dgvPodaci.RowHeadersWidth = 51;
+            dgvPodaci.RowTemplate.Height = 29;
+            dgvPodaci.Size = new Size(776, 265);
+            dgvPodaci.TabIndex = 2;
+            // 
+            // Ime
+            // 
+            Ime.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Ime.DataPropertyName = "Ime";
+            Ime.HeaderText = "Ime";
+            Ime.MinimumWidth = 6;
+            Ime.Name = "Ime";
+            Ime.ReadOnly = true;
+            // 
+            // Prezime
+            // 
+            Prezime.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Prezime.DataPropertyName = "Prezime";
+            Prezime.HeaderText = "Prezime";
+            Prezime.MinimumWidth = 6;
+            Prezime.Name = "Prezime";
+            Prezime.ReadOnly = true;
+            // 
+            // Grad
+            // 
+            Grad.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Grad.DataPropertyName = "Grad";
+            Grad.HeaderText = "Grad";
+            Grad.MinimumWidth = 6;
+            Grad.Name = "Grad";
+            Grad.ReadOnly = true;
+            // 
+            // Drzava
+            // 
+            Drzava.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Drzava.DataPropertyName = "Drzava";
+            Drzava.HeaderText = "Drzava";
+            Drzava.MinimumWidth = 6;
+            Drzava.Name = "Drzava";
+            Drzava.ReadOnly = true;
+            // 
+            // Prosjek
+            // 
+            Prosjek.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Prosjek.DataPropertyName = "Prosjek";
+            Prosjek.HeaderText = "Prosjek";
+            Prosjek.MinimumWidth = 6;
+            Prosjek.Name = "Prosjek";
+            Prosjek.ReadOnly = true;
+            // 
+            // frmPretraga
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 354);
+            Controls.Add(dgvPodaci);
+            Controls.Add(cmbGrad);
+            Controls.Add(cmbDrzava);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmPretraga";
+            Text = "Podaci o studentima";
+            Load += frmPretraga_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvPodaci).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private ComboBox cmbDrzava;
+        private Label label2;
+        private ComboBox cmbGrad;
+        private DataGridView dgvPodaci;
+        private DataGridViewTextBoxColumn Ime;
+        private DataGridViewTextBoxColumn Prezime;
+        private DataGridViewTextBoxColumn Grad;
+        private DataGridViewTextBoxColumn Drzava;
+        private DataGridViewTextBoxColumn Prosjek;
     }
 }
--- PRIII/01.02.2024/FIT.WinForms/IB220240/frmPretraga.cs
+++ PRIII/01.02.2024/FIT.WinForms/IB220240/frmPretraga.cs
@@ -7,14 +7,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FIT.Data;
+using FIT.Data.IB220240;
+using FIT.Infrastructure;
 
 namespace FIT.WinForms.IB220240
 {
     public partial class frmPretraga : Form
     {
+        private static DLWMSDbContext db = Shared.DLWMSDbContext;
+        List<Student> studenti = new List<Student>();
         public frmPretraga()
         {
             InitializeComponent();
+            dgvPodaci.AutoGenerateColumns = false;
+        }
+
+        private void frmPretraga_Load(object sender, EventArgs e)
+        {
+            cmbDrzava.DataSource = db.Drzave.ToList();
+            var drzava = cmbDrzava.SelectedItem as Drzave;
+            cmbGrad.DataSource = db.Gradovi.Where(g => g.DrzavaId == drzava.Id).ToList();
+            UcitajPodatke();
+        }
+
+        private void UcitajPodatke()
+        {
+            var drzava = cmbDrzava.SelectedItem as Drzave;
+            var grad = cmbGrad.SelectedItem as Gradovi;
+            studenti = db.Studenti.Where(s => s.GradId == grad.Id).ToList();
+
+            if (studenti.Count == 0) { MessageBox.Show($"U bazi nisu evidentirani studenti koji je rodjen u {grad}, {drzava}!", "Obavjest"); }
+
+            var tabela = new DataTable();
+            tabela.Columns.Add("Ime");
+            tabela.Columns.Add("Prezime");
+            tabela.Columns.Add("Grad");
+            tabela.Columns.Add("Drzava");
+            tabela.Columns.Add("Prosjek");
+
+            for (int i = 0; i < studenti.Count; i++)
+            {
+                var student = studenti[i];
+                var red = tabela.NewRow();
+
+                red["Ime"] = student.Ime;
+                red["Prezime"] = student.Prezime;
+                red["Grad"] = student.Grad;
+                red["Drzava"] = drzava;
+                red["Prosjek"] = db.PolozeniPredmeti.Where(p => p.StudentId == student.Id).Count() == 0 ? "5" : db.PolozeniPredmeti.Where(p => p.StudentId == student.Id).Average(x => x.Ocjena);
+
+                tabela.Rows.Add(red);
+            }
+            dgvPodaci.DataSource = null;
+            dgvPodaci.DataSource = tabela;
+        }
+
+        private void cmbDrzava_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            var drzava = cmbDrzava.SelectedItem as Drzave;
+            cmbGrad.DataSource = db.Gradovi.Where(g => g.DrzavaId == drzava.Id).ToList();
+            UcitajPodatke();
+        }
+
+        private void cmbGrad_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            UcitajPodatke();
         }
     }
 }
